f959bf5b4c93cb98b1eaf37e27e313ebfa3fc866
谢俊鸿 <xiejunhong@coding.net>
2020-08-30 12:59:20 +0800

    init

---
+++ index-esm.mjs
@@ -0,0 +1,64 @@
+import * as monaco from "/node_modules/monaco-editor/esm/vs/editor/editor.main.js";
+
+(async function () {
+    const read = function (path) {
+        return new Promise((resolve) => {
+            fetch(path)
+                .then((r) => r.text())
+                .then((res) => {
+                    resolve(res);
+                });
+        });
+    };
+
+    // The diff editor offers a navigator to jump between changes. Once the diff is computed the <em>next()</em> and <em>previous()</em> method allow navigation. By default setting the selection in the editor manually resets the navigation state.
+    var originalModel = monaco.editor.createModel(await read("./assets/test.js"));
+    var modifiedModel = monaco.editor.createModel(await read("./assets/test-diff.js"));
+
+    // const myeditor = monaco.editor.create(document.getElementById("container"), {
+    //     value: await read("./assets/test.js"),
+    //     language: "foldLanguage",
+    //     codeLens: true,
+    //     folding: true,
+    //     showFoldingControls: "always"
+    // });
+
+    // monaco.editor.setModelLanguage(originalModel, "javascript");
+    // monaco.editor.setModelLanguage(modifiedModel, "javascript");
+    monaco.languages.registerFoldingRangeProvider("javascript", {
+        provideFoldingRanges: function (model, context, token) {
+            console.log(arguments);
+            return [
+                // comment1
+                {
+                    start: 5,
+                    end: 20,
+                    kind: monaco.languages.FoldingRangeKind.Comment,
+                },
+            ];
+        },
+    });
+
+    monaco.editor.create(document.getElementById("container"), {
+        model: originalModel,
+        folding: true
+    });
+
+    var diffEditor = monaco.editor.createDiffEditor(document.getElementById("diff"), {
+        codeLens: true,
+        folding: true,
+        showFoldingControls: "always",
+    });
+
+    diffEditor.setModel({
+        original: originalModel,
+        modified: modifiedModel,
+    });
+
+    var navi = monaco.editor.createDiffNavigator(diffEditor, {
+        followsCaret: true, // resets the navigator state when the user selects something in the editor
+        ignoreCharChanges: true, // jump from line to line
+    });
+
+    navi.next();
+})();
---
+++ node.js
@@ -0,0 +1,27 @@
+const connect = require("connect");
+const http = require("http");
+const static = require("serve-static");
+const transform = require("connect-static-transform");
+
+const ignore = "browser/ui/codiconLabel/codicon/codicon.css";
+
+const css = transform({
+    root: __dirname,
+    match: /node_modules\/(.+)\.css/,
+    transform: (path, content, send) => {
+        const exec = (c) => (path.includes(ignore) ? c.replace(/:before { content: "/g, `:before { content: "\\`) : c);
+        const c = `
+        const css = document.createElement('style');
+        css.type = "text/css";
+        css.textContent = \`${exec(content)}\`;
+        document.head.appendChild(css);
+        `;
+        send(c, { "Content-Type": "application/javascript" });
+    },
+});
+
+const app = connect().use(css).use(static(__dirname));
+
+http.createServer(app).listen(8081);
+
+console.log("http://127.0.0.1:8081");
